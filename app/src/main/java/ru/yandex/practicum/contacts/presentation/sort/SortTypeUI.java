5be60d410f2c93ec28391bf7fb1f17bfb784ca9e
Anton Vyshkin <a.vyshkin@space307.com>
2022-09-20 11:40:15 +0300

    replace static inner class ListDiffCallback to BaseListDiffCallback

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.contacts.presentation.sort;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeUI {
+public class SortTypeUI implements ListDiffInterface<SortTypeUI> {
 
     private final SortType sortType;
     private final boolean selected;
@@ -20,6 +23,11 @@ public class SortTypeUI {
 
     public boolean isSelected() {
         return selected;
+    }
+
+    @Override
+    public boolean theSameAs(SortTypeUI sortTypeUI) {
+        return this.getSortType() == sortTypeUI.getSortType();
     }
 
     @Override
